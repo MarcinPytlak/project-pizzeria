998c0253aca35394f8484d236dde019467d45585
Marcin Pytlak <marcin.pytlak@o2.pl>
2021-01-20 21:58:04 +0100

    11.3 in progress

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import {select, templates, settings} from '../settings.js';
+import {select, templates, settings, classNames} from '../settings.js';
 import utils from '../utils.js'; 
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
@@ -6,6 +6,7 @@ import HourPicker from './HourPicker.js';
 class Booking{
   constructor(element){
     const thisBooking = this;
+    thisBooking.tableSelected = [];
     thisBooking.render(element);
     thisBooking.initWidget();
     thisBooking.getData();
@@ -54,10 +55,82 @@ class Booking{
         ]);
       })
       .then(function([bookings, eventsCurrentResponse, eventsRepeatResponse]){
-        console.log(bookings, eventsCurrentResponse, eventsRepeatResponse);
+        // console.log(bookings, eventsCurrentResponse, eventsRepeatResponse);
+        thisBooking.parseData(bookings, eventsCurrentResponse, eventsRepeatResponse);
       });
 
   }
+  parseData(bookings, eventsCurrent, eventsRepeat){
+    const thisBooking = this;
+    thisBooking.booked = {};
+    for(let item of eventsCurrent){
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+    for(let item of bookings){
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+    const minDate = thisBooking.pickedDate.minDate;
+    const maxDate = thisBooking.pickedDate.maxDate;
+    for(let item of eventsRepeat){
+      if(item.repeat == 'daily'){
+        for(let loopDate = minDate; loopDate <= maxDate; loopDate = utils.addDays(loopDate, 1)){
+          thisBooking.makeBooked(utils.dateToStr(loopDate), item.hour, item.duration, item.table);
+        }
+      }
+    }
+    // console.log(thisBooking.booked);
+    thisBooking.updateDOM();
+  }
+  
+  makeBooked(date, hour, duration, table){
+    const thisBooking = this;
+    if(typeof thisBooking.booked[date] == 'undefined'){
+      thisBooking.booked[date] = {};
+    }
+    const startHour = utils.hourToNumber(hour);
+
+    for(let hourBlock = startHour; hourBlock < startHour + duration; hourBlock += 0.5){
+      // console.log('loop', hourBlock);
+ 
+      if(typeof thisBooking.booked[date][hourBlock] == 'undefined'){
+        thisBooking.booked[date][hourBlock] = [];
+      }
+      thisBooking.booked[date][hourBlock].push(table);
+    }
+  }
+  updateDOM(){
+    const thisBooking = this;
+
+    thisBooking.date = thisBooking.pickedDate.value;
+    thisBooking.hour = utils.hourToNumber(thisBooking.pickedHour.value);
+
+    let allAvailable = false;
+
+    if(
+      typeof thisBooking.booked[thisBooking.date] == 'undefined'
+      ||
+      typeof thisBooking.booked[thisBooking.date][thisBooking.hour] == 'undefined'
+    ){
+      allAvailable = true;
+    }
+   
+    for(let table of thisBooking.dom.tables){
+      let tableId = table.getAttribute(settings.booking.tableIdAttribute);
+      if(!isNaN(tableId)){
+        tableId = parseInt(tableId);
+      }
+      if(
+        !allAvailable
+        &&
+        thisBooking.booked[thisBooking.date][thisBooking.hour].includes(tableId)
+      ){
+        table.classList.add(classNames.booking.tableBooked);
+      }else{
+        table.classList.remove(classNames.booking.tableBooked);
+      }
+    }
+  }
+
   render(element){
     const thisBooking = this;
     const generatedHTML = templates.bookingWidget();
@@ -70,6 +143,8 @@ class Booking{
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
     thisBooking.dom.pickedDate = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
     thisBooking.dom.pickedHour = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
+    thisBooking.dom.tables = thisBooking.dom.wrapper.querySelectorAll(select.booking.tables);
+    thisBooking.dom.totalTables = thisBooking.dom.wrapper.querySelector(select.booking.allTables);
   }
   initWidget(){
     const thisBooking = this;
@@ -83,6 +158,53 @@ class Booking{
     thisBooking.dom.hoursAmount.addEventListener('updated', function(){
       console.log(thisBooking.dom.hoursAmount);
     });
+    thisBooking.dom.wrapper.addEventListener('updated', function(){
+      thisBooking.updateDOM();
+    });
+    thisBooking.dom.totalTables.addEventListener('click', function(clickedElement){
+      thisBooking.initTables(clickedElement);
+
+      
+    });
+  }
+  initTables(tables){
+    const thisBooking = this;
+    thisBooking.allTables = tables.path[1];
+    thisBooking.tables = tables.path[0];
+    console.log(thisBooking.tables);
+    if(thisBooking.tables.getElementsByClassName('object')){
+      if(thisBooking.tables.classList == 'booked'){
+        console.log('this table is booked');
+      }else{
+        let tableId = thisBooking.tables.getAttribute('data-table');
+        thisBooking.tableSelected.push(tableId);
+
+        if(thisBooking.allTables.getElementsByClassName('selected') == true){
+          thisBooking.tables.classList.remove('selected');
+        } else {
+          thisBooking.tables.classList.add('selected');
+        }
+
+      }
+    }
+    
+  
+    // if(thisBooking.tables.classList == 'object table'){
+    //   if(thisBooking.tables.classList == 'booked'){
+    //     console.log('this table is booked');
+    //   } else {
+    //     let tableId = thisBooking.table.getAttribute('data-table');
+    //     thisBooking.tableSelected.push(tableId);
+    //     thisBooking.table.classList.add('selected');
+    //     if(thisBooking.dom.totalTables.getElementsByClassName('selected') == true){
+    //       thisBooking.table.classList.remove('selected');
+    //     }else {
+    //       thisBooking.table.classList.add('selected');
+    //     }
+    //   }
+
+    // }
+
   }
 }
 export default Booking;
--- src/js/settings.js
+++ src/js/settings.js
@@ -63,6 +63,8 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    allTables: '.floor-plan',
+
   },
   nav: {
     links: '.main-nav a',
